5cab37ad8cd85e117c0315ee6ba3a9ecdaaedc94
Poliana Abadio <polianabadio@gmail.com>
2023-12-04 17:59:25 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/fila_facil/CadastrarFila.java
@@ -0,0 +1,14 @@
+package com.example.fila_facil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadastrarFila extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_fila);
+    }
+}
